52c18aad68f59b3401b48d6554dcbc988e686ced
Piotr Rusin <piotr.rusin88@gmail.com>
2016-06-11 14:12:53 +0200

    Add AbstractMappedPost class
    The class is being added to AppBundle\Entity namespace for convenience -
    it is not a doctrine entity, but a mapped superclass.

---
+++ src/AppBundle/Entity/AbstractMappedPost.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * Copyright 2016 Piotr Rusin <piotr.rusin88@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A base class for entities representing postable content
+ * (articles, comments, etc.).
+ *
+ * @ORM\MappedSuperclass
+ *
+ * @author Piotr Rusin <piotr.rusin88@gmail.com>
+ */
+abstract class AbstractMappedPost
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     */
+    protected $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\DateTime()
+     */
+    protected $publicationDate;
+
+    public function __construct()
+    {
+        $this->publicationDate = new \DateTime();
+    }
+
+    /**
+     * Set publicationDate.
+     *
+     * @param \DateTime $publicationDate
+     *
+     * @return MappedPost
+     */
+    public function setPublicationDate(\DateTime $publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate.
+     *
+     * @return \DateTime
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content.
+     *
+     * @param string $content
+     *
+     * @return MappedPost
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
