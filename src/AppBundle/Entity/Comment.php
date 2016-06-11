d3e3b312f5bfc482c135a724913ed0a35561f3c8
Piotr Rusin <piotr.rusin88@gmail.com>
2016-06-11 14:16:13 +0200

    Add Comment entity class

---
+++ src/AppBundle/Entity/Comment.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * Copyright 2016 Piotr Rusin <piotr.rusin88@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A class representing user comments associated with blog articles.
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="comments")
+ *
+ * @author Piotr Rusin <piotr.rusin88@gmail.com>
+ */
+class Comment extends AbstractMappedPost
+{
+    /**
+     * Name of unregistered author.
+     *
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $authorName;
+
+    /**
+     * Email address of unregistered author.
+     *
+     * @ORM\Column(type="string")
+     * @Assert\Email()
+     */
+    private $authorEmail;
+
+    /**
+     * Set authorName.
+     *
+     * @param string $authorName
+     *
+     * @return Comment
+     */
+    public function setAuthorName($authorName)
+    {
+        $this->authorName = $authorName;
+
+        return $this;
+    }
+
+    /**
+     * Get authorName.
+     *
+     * @return string
+     */
+    public function getAuthorName()
+    {
+        return $this->authorName;
+    }
+
+    /**
+     * Set authorEmail.
+     *
+     * @param string $authorEmail
+     *
+     * @return Comment
+     */
+    public function setAuthorEmail($authorEmail)
+    {
+        $this->authorEmail = $authorEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get authorEmail.
+     *
+     * @return string
+     */
+    public function getAuthorEmail()
+    {
+        return $this->authorEmail;
+    }
+}
