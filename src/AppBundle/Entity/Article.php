7330d6d07485a41434c76e8bf7ccc7a71a34e800
Piotr Rusin <piotr.rusin88@gmail.com>
2016-06-11 14:14:24 +0200

    Add Article entity class

---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Copyright 2016 Piotr Rusin <piotr.rusin88@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * A class representing blog articles.
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="articles")
+ *
+ * @author Piotr Rusin <piotr.rusin88@gmail.com>
+ */
+class Article extends AbstractMappedPost
+{
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $slug;
+
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug.
+     *
+     * @param string $slug
+     *
+     * @return Article
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug.
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
