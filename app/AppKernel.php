001aa29b3d85df77cae335333537d68c3d53b997
Piotr Rusin <piotr.rusin88@gmail.com>
2016-08-01 20:44:52 +0200

    Add TetranzSelect2EntityBundle to active bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
